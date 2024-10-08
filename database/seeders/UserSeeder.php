27018b52a230ee1584444a55657d3e2ebfe5b685
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-10-08 15:08:47 +0600

    added role-permission | template mastering

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+
+        $admin = Role::firstOrCreate(['name' => 'admin']);
+
+        //creating a admin account
+        $AdminCount = new User;
+        $AdminCount->name = 'Admin';
+        $AdminCount->email = 'admin@gmail.com';
+        $AdminCount->password =  Hash::make('123');
+        $AdminCount->save();
+
+        $AdminCount->assignRole($admin);
+    }
+}
